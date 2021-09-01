b645efe528ef445df2d5abb8c5c6e9677d7d5e32
e5624267 <e5624267@PNQ1-LP8C5SX33.FNFIS.com>
2021-09-01 18:51:55 +0530

    vcmttt

--- Selenium/src/Practice.java
+++ Selenium/src/Practice.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -21,18 +23,21 @@ public class Practice {
 		WebDriverWait wt = new WebDriverWait(dr, 10);
 		WebElement formDemo = wt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Simple Form Demo')]")));
 		formDemo.click(); 
+		dr.navigate().to("https://www.google.com");
+		Assert.assertTrue(formDemo.isDisplayed());
 		dr.findElement(By.id("user-message")).sendKeys("Hello All");
 		dr.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
-		SoftAssert sfasrt = new SoftAssert();
-		sfasrt.
+		SoftAssert sf = new SoftAssert();
+		sf.assertTrue(dr.findElement(By.id("")).isDisplayed());
+		Actions act = new Actions(dr);
 		
 	}
-	
+
 	@Test
 	public void testOne()
 	{
-		
-		
+
+
 	}
 
 }
