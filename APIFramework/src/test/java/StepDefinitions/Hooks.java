80d3e042f3572ae487355225cd914ef4e5db6979
e5624267 <e5624267@PNQ1-LP8C5SX33.FNFIS.com>
2021-09-17 21:12:17 +0530

    db

--- APIFramework/src/test/java/StepDefinitions/Hooks.java
+++ APIFramework/src/test/java/StepDefinitions/Hooks.java
@@ -13,7 +13,7 @@ public class Hooks {
 //IN MY OFFICE PROJECT : in case if i want to delete a merchant then a merchant should be first present in application 
 //then we can delete it. in end to end test we create merchant then we update and then delete. but if i want to just only 
 // run delete feature alone, then I have to write create merchant code in hooks so that we will have merchant id to delete
-//		
+
 
 
 	}
