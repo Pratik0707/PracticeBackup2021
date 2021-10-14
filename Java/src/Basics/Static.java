04d8cdf28dcf6688d6faa742aa284f79358a34ce
e5624267 <e5624267@PNQ1-LP8C5SX33.FNFIS.com>
2021-10-14 18:34:03 +0530

    fdgdf

--- Java/src/Basics/Static.java
+++ Java/src/Basics/Static.java
@@ -6,9 +6,11 @@ public class Static {
 		// static int a=10; cann not have static variable inside static methood
 		demo1();// static, called without object
 		Static st = new Static();
+		st.demo1('p');// static method overloading
+		st.demo1(10);// static method overloading
 		st.nonStatic();// need object to call non static method
 		System.out.println(i);// access static variable directly
-		System.out.println(r);// cannot access non-static variable directly
+	//	System.out.println(r);// cannot access non-static variable directly
 
 	}
 	static int i=0;
@@ -21,6 +23,16 @@ public class Static {
 	void nonStatic()
 	{
 
+	}
+	
+	static void demo1(int i)
+	{
+		System.out.println(i);
+	}
+
+	static void demo1(char c)
+	{
+		System.out.println(c);
 	}
 
 }
